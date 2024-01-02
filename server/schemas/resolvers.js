3667ecbafca349dd877aab47c9d6682d2ac5d136
Aston Dahllöf <Aston_dahllof@outlook.com>
2024-01-02 18:41:20 -0400

    Added saveBook and removeBook. Running into issue with removeBook

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,17 +14,52 @@ const resolvers = {
       const user = await User.create({username, email, password});
       const token = signToken(user);
 
-      console.log('Logged in user is:', user);
+      console.log('Added user is:', user);
 
       return {token, user};
       //Ensure above works. Mutations are weird. As their name implies
     },
     
     //login function here
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw  AuthenticationError('Incorrect email');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      console.log('Correct password:', password);
+      if (!correctPw) {
+        throw  AuthenticationError('Incorrect password');
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
 
     //savebook function here
+    saveBook: async (_, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: input } },
+          { new: true }
+        ).populate('savedBooks');
+        return updatedUser;
+      }
+      throw AuthenticationError('Not logged in');
+    },
 
     //remove book function here
+    removeBook: async (_, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: input } },
+          { new: true }
+        ).populate('savedBooks');
+        return updatedUser;
+      }
+      throw AuthenticationError('Not logged in');
+    },
 
   },
 };
